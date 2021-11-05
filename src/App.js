380b292bfed1ea42a27a16268a36d6a8396a03e4
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-05 10:43:41 -0400

    creates context, needs to add functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  Header,
+  Inputs,
+  Lists
+} from "./components";
 
 import "./App.css";
 
 function App() {
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  const [option, setOption] = useState("+");
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const [incomeArray, setIncomeArray] = useState([]);
+  const [expenseArray, setExpenseArray] = useState([]);
   return (
-    <div>
-      
+    <div className="App">
+      <Header />
+      <Inputs />
+      <Lists />
     </div>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./header";
+import Inputs from "./inputs";
+import Lists from "./lists";
+
+export {
+   Header,
+   Inputs,
+   Lists,
+};
